ffbedd7a2dbbbd4bbfb816276d0cdfd274658496
Philipe Fatio <me@phili.pe>
2013-02-09 17:17:47 +0100

    Don't run SimpleCov on Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
-require 'simplecov'
-SimpleCov.start do
-  add_filter '/spec'
-  add_filter '/example'
+unless ENV['CI']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter '/spec'
+    add_filter '/example'
+  end
 end
 
 require 'rack/test'
