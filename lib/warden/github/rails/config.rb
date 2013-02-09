4667fbc3bb7b85edf688a32686c46eb1ad9e9c38
Philipe Fatio <me@phili.pe>
2013-02-09 18:14:27 +0100

    Add scopes configs

--- lib/warden/github/rails/config.rb
+++ lib/warden/github/rails/config.rb
@@ -7,6 +7,17 @@ module Warden
         # Default scope to use when not explicitly specified.
         mattr_accessor :default_scope
         @@default_scope = :user
+
+        # The list of scopes and their configs. This is used to add custom
+        # configs to a specific scope. When using a scope that is not listed
+        # here, it will use the default configs from warden-github.
+        mattr_accessor :scopes
+        @@scopes = {}
+
+        # Adds a scope with custom configurations to the list of scopes.
+        def self.add_scope(name, config={})
+          scopes[name] = config
+        end
       end
     end
   end
--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -6,4 +6,18 @@ describe Warden::GitHub::Rails::Config do
       described_class.default_scope.should be :user
     end
   end
+
+  describe '#scopes' do
+    it 'defaults to an empty hash' do
+      described_class.scopes.should == {}
+    end
+  end
+
+  describe '#add_scope' do
+    it 'adds a scope with its configs' do
+      config = double
+      described_class.add_scope :admin, config
+      described_class.scopes[:admin].should eq config
+    end
+  end
 end
