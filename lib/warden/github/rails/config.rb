6e6172abfc9ef645317c31eb6f57d9338b2f3a35
Philipe Fatio <me@phili.pe>
2013-02-11 15:04:00 +0100

    Add team configs

--- lib/warden/github/rails/config.rb
+++ lib/warden/github/rails/config.rb
@@ -2,6 +2,8 @@ module Warden
   module GitHub
     module Rails
       class Config
+        BadConfig = Class.new(StandardError)
+
         # A cache of the warden config for the active manager.
         attr_accessor :warden_config
 
@@ -13,14 +15,29 @@ module Warden
         # here, it will use the default configs from warden-github.
         attr_reader :scopes
 
+        # A hash containing team alias names and their numeric id.
+        attr_reader :teams
+
         def initialize
-          @scopes = {}
           @default_scope = :user
+          @scopes = {}
+          @teams = {}
         end
 
         # Adds a scope with custom configurations to the list of scopes.
         def add_scope(name, config={})
           scopes[name] = config
+        end
+
+        # Maps a team id to a name in order to easier reference it.
+        def add_team(name, id)
+          teams[name.to_sym] = Integer(id)
+        end
+
+        def team_id(team)
+          Integer(team)  rescue teams.fetch(team.to_sym)
+        rescue IndexError
+          fail BadConfig, "No alias defined for team #{team}."
         end
       end
     end
--- spec/unit/config_spec.rb
+++ spec/unit/config_spec.rb
@@ -15,11 +15,53 @@ describe Warden::GitHub::Rails::Config do
     end
   end
 
+  describe '#teams' do
+    it 'defaults to an empty hash' do
+      config.scopes.should == {}
+    end
+  end
+
   describe '#add_scope' do
     it 'adds a scope with its configs' do
       scope_config = double
       config.add_scope :admin, scope_config
       config.scopes[:admin].should eq scope_config
+    end
+  end
+
+  describe '#add_team' do
+    it 'adds a name mapping for a team' do
+      config.add_team :marketing, 1234
+      config.teams[:marketing].should eq 1234
+    end
+
+    it 'normalizes the input' do
+      config.add_team 'marketing', '1234'
+      config.teams[:marketing].should eq 1234
+    end
+  end
+
+  describe '#team_id' do
+    context 'when passed a numeric value' do
+      it 'returns that value' do
+        config.team_id('1234').should eq 1234
+        config.team_id(1234).should eq 1234
+      end
+    end
+
+    context 'when passed an alias' do
+      it 'returns the id for the alias' do
+        config.add_team :marketing, 1234
+        config.team_id(:marketing).should eq 1234
+        config.team_id('marketing').should eq 1234
+      end
+    end
+
+    context 'when no mapping exists' do
+      it 'raises a BadConfig' do
+        expect { config.team_id(:foobar) }.
+          to raise_error(Warden::GitHub::Rails::Config::BadConfig)
+      end
     end
   end
 end
