6bce9f36acdcf2a3afed6aaa198c2ffdf0501458
Philipe Fatio <me@phili.pe>
2013-02-11 07:41:40 +0100

    Fix typo

--- spec/unit/rails_spec.rb
+++ spec/unit/rails_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Warden::GitHub::Rails do
   describe '.setup' do
-    it 'yields the a config instance' do
+    it 'yields a config instance' do
       described_class.setup do |config|
         config.should be_a described_class::Config
       end
