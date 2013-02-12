d3a309ecaffbe43f49c1a26d0f06bb3e5d2ae766
Philipe Fatio <me@phili.pe>
2013-02-12 10:25:55 +0100

    Add documentation about memberships

--- lib/warden/github/rails/routes.rb
+++ lib/warden/github/rails/routes.rb
@@ -4,6 +4,9 @@ module Warden
       module Routes
         # Enforces an authenticated GitHub user for the routes. If not
         # authenticated, it initiates the OAuth flow.
+        #
+        # Team and organization memberships can be checked by specifying a hash
+        # such as `:team => 'foobar'` or `:org => 'my_company'`.
         def github_authenticate(scope=nil, options={}, &routes_block)
           github_constraint(scope, options, routes_block) do |warden, scope|
             warden.authenticate!(:scope => scope)
@@ -12,6 +15,9 @@ module Warden
 
         # The routes will only be visible to authenticated GitHub users. When
         # not authenticated, it does not initiate the OAuth flow.
+        #
+        # Team and organization memberships can be checked by specifying a hash
+        # such as `:team => 'foobar'` or `:org => 'my_company'`.
         def github_authenticated(scope=nil, options={}, &routes_block)
           github_constraint(scope, options, routes_block) do |warden, scope|
             warden.authenticated?(:scope => scope)
@@ -19,6 +25,9 @@ module Warden
         end
 
         # The routes will only be visible to all but authenticated GitHub users.
+        #
+        # This constraint currently does not check for memberships since of its
+        # limited usage.
         def github_unauthenticated(scope=nil, options={}, &routes_block)
           github_constraint(scope, options, routes_block) do |warden, scope|
             not warden.authenticated?(:scope => scope)
