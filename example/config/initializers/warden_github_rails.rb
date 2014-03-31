a4dee4193e73ef91b3d645b6d2011f776bbc9c12
Philipe Fatio <me@phili.pe>
2014-03-31 19:37:13 +0200

    Use less invasive scopes
    Thanks @atmos
    Closes #9

--- example/config/initializers/warden_github_rails.rb
+++ example/config/initializers/warden_github_rails.rb
@@ -1,3 +1,3 @@
 Warden::GitHub::Rails.setup do |config|
-  config.add_scope :user, redirect_uri: '/login', scope: 'user,repo'
+  config.add_scope :user, redirect_uri: '/login', scope: 'user:email,read:org'
 end
