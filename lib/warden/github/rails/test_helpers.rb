4ed4c1c3903b7e3971447a76e8aea8bf14a24950
Philipe Fatio <me@phili.pe>
2014-09-17 11:17:23 +0200

    Define test helper in proper place

--- lib/warden/github/rails/test_helpers.rb
+++ lib/warden/github/rails/test_helpers.rb
@@ -18,11 +18,10 @@ module Warden
   end
 end
 
-# Add a method to Rack::Response to easily determine if a request resulted in an
-# OAuth redirect to GitHub.
-class Rack::Response
+# Add a method to Rack::Response::Helpers to easily determine if a request
+# resulted in an OAuth redirect to GitHub.
+module Rack::Response::Helpers
   def github_oauth_redirect?
-    redirect? and
-      location.start_with?('https://github.com/login/oauth/authorize')
+    redirect? && %r{https?://[^/]+/login/oauth/authorize\b} =~ location
   end
 end
